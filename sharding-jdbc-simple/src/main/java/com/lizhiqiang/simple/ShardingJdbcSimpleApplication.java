b452eba417b275ad53779abadb4eeaf53128cd8e
736465175 <736465175@qq.com>
2021-03-21 00:32:48 +0800

    初始提交

---
+++ sharding-jdbc-simple/src/main/java/com/lizhiqiang/simple/ShardingJdbcSimpleApplication.java
@@ -0,0 +1,20 @@
+package com.lizhiqiang.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 侯文远
+ * @version V1.0
+ * @Package com.lizhiqiang.simple
+ * @date 2021-03-21 00:28
+ * @Copyright © 李志强
+ */
+
+@SpringBootApplication
+public class ShardingJdbcSimpleApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(ShardingJdbcSimpleApplication.class, args);
+    }
+}
