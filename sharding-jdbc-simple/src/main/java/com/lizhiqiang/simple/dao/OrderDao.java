251fd8504c0006ec5e243ef497e707191d634bde
lizhiqiang <lizhiqiang@cnhis.com>
2021-04-13 17:09:32 +0800

    bug

--- sharding-jdbc-simple/src/main/java/com/lizhiqiang/simple/dao/OrderDao.java
+++ sharding-jdbc-simple/src/main/java/com/lizhiqiang/simple/dao/OrderDao.java
@@ -18,5 +18,5 @@ public interface OrderDao {
 
     //定义分片规则的时候已经定义主键自动生成，不需要传入；这里表写逻辑表，sharding-jdbc会自动帮你根据分片策略找到表
     @Insert("insert into t_order(price,user_id,status) values(#{price},#{userId},#{status})")
-    int insertOrder(@Param("price")BigDecimal price,@Param("user_id")Long userId,@Param("status")String status);
+    int insertOrder(@Param("price")BigDecimal price,@Param("userId")Long userId,@Param("status")String status);
 }
